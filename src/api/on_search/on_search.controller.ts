62a6379226414bdc864409502fcdfcb3c9b179e0
ssakethraju <ssakethraj@gmail.com>
2023-09-13 18:51:15 -0500

    add console

--- src/api/on_search/on_search.controller.ts
+++ src/api/on_search/on_search.controller.ts
@@ -7,6 +7,7 @@ import { OnSearchDTO } from './on_search.dto';
 
 export default function onSearchController(req: Request, res: Response) {
   const onSearchDto: OnSearchDTO = req.body;
+	console.log('Search callback triggered');
   sendAcknowledgement(res, 'ACK');
   return requestForwarder(onSearchDto);
 }
